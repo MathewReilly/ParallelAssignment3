b9c3ed9907c17ae89c835681f66b4a68e1e024c1
Mathew-Reilly <mathewreilly2735@outlook.com>
2024-04-04 00:57:49 -0400

    file skeletons

---
+++ Assignment3P1.cpp
@@ -0,0 +1,39 @@
+// Mathew Reilly
+// COP4520
+// Assignment 3
+// Description:
+//    Gifts and linked chains
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <vector>
+#include <chrono>
+#include <fstream>
+using namespace std;
+
+class Present
+{
+public:
+  int tag;
+  Present* nextPresent;
+}
+
+// USE lazy???
+
+
+void guest(int type, int number)
+{
+  
+}
+
+
+int main()
+{
+  // initializing variables for the threads to interact with
+  
+
+  return 0;
+}
+
+
---
+++ Assignment3P2.cpp
@@ -0,0 +1,32 @@
+// Mathew Reilly
+// COP4520
+// Assignment 3
+// Description:
+//    BOUNDED QUEUE QUESTION
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <vector>
+#include <chrono>
+#include <fstream>
+using namespace std;
+
+//USE A BOUNDED QUEUE
+
+// some function def
+void guest(int type, int number)
+{
+  
+}
+
+
+int main()
+{
+  // initializing variables for the threads to interact with
+  
+
+  return 0;
+}
+
+
